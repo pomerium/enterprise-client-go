d8048381f24f1712a00daea8e145c6c1c33a11f5
Denis Mishin <dmishin@pomerium.com>
2025-03-06 14:55:45 -0500

    pre-v0.29 api update (#80)

--- go.mod
+++ go.mod
@@ -5,48 +5,23 @@ go 1.23.0
 toolchain go1.23.1
 
 require (
-	github.com/envoyproxy/go-control-plane/envoy v1.32.4
 	github.com/envoyproxy/protoc-gen-validate v1.2.1
-	github.com/pomerium/pomerium v0.28.1-0.20250218200206-b9fd926618e2
 	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.70.0
 	google.golang.org/protobuf v1.36.4
 )
 
 require (
-	github.com/caddyserver/certmagic v0.21.4 // indirect
-	github.com/caddyserver/zerossl v0.1.3 // indirect
-	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
-	github.com/cncf/xds/go v0.0.0-20241223141626-cff3c89139a3 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-set/v3 v3.0.0 // indirect
-	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
-	github.com/jxskiss/base62 v1.1.0 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
-	github.com/libdns/libdns v0.2.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mholt/acmez/v2 v2.0.3 // indirect
-	github.com/miekg/dns v1.1.62 // indirect
-	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/pomerium/protoutil v0.0.0-20240813175624-47b7ac43ff46 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
-	github.com/zeebo/blake3 v0.2.4 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/mod v0.20.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.33.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/tools v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pb/types.pb.go
@@ -0,0 +1,419 @@
+//
+//
+//Apache License
+//Version 2.0, January 2004
+//http://www.apache.org/licenses/
+//
+//TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+//
+//1. Definitions.
+//
+//"License" shall mean the terms and conditions for use, reproduction,
+//and distribution as defined by Sections 1 through 9 of this document.
+//
+//"Licensor" shall mean the copyright owner or entity authorized by
+//the copyright owner that is granting the License.
+//
+//"Legal Entity" shall mean the union of the acting entity and all
+//other entities that control, are controlled by, or are under common
+//control with that entity. For the purposes of this definition,
+//"control" means (i) the power, direct or indirect, to cause the
+//direction or management of such entity, whether by contract or
+//otherwise, or (ii) ownership of fifty percent (50%) or more of the
+//outstanding shares, or (iii) beneficial ownership of such entity.
+//
+//"You" (or "Your") shall mean an individual or Legal Entity
+//exercising permissions granted by this License.
+//
+//"Source" form shall mean the preferred form for making modifications,
+//including but not limited to software source code, documentation
+//source, and configuration files.
+//
+//"Object" form shall mean any form resulting from mechanical
+//transformation or translation of a Source form, including but
+//not limited to compiled object code, generated documentation,
+//and conversions to other media types.
+//
+//"Work" shall mean the work of authorship, whether in Source or
+//Object form, made available under the License, as indicated by a
+//copyright notice that is included in or attached to the work
+//(an example is provided in the Appendix below).
+//
+//"Derivative Works" shall mean any work, whether in Source or Object
+//form, that is based on (or derived from) the Work and for which the
+//editorial revisions, annotations, elaborations, or other modifications
+//represent, as a whole, an original work of authorship. For the purposes
+//of this License, Derivative Works shall not include works that remain
+//separable from, or merely link (or bind by name) to the interfaces of,
+//the Work and Derivative Works thereof.
+//
+//"Contribution" shall mean any work of authorship, including
+//the original version of the Work and any modifications or additions
+//to that Work or Derivative Works thereof, that is intentionally
+//submitted to Licensor for inclusion in the Work by the copyright owner
+//or by an individual or Legal Entity authorized to submit on behalf of
+//the copyright owner. For the purposes of this definition, "submitted"
+//means any form of electronic, verbal, or written communication sent
+//to the Licensor or its representatives, including but not limited to
+//communication on electronic mailing lists, source code control systems,
+//and issue tracking systems that are managed by, or on behalf of, the
+//Licensor for the purpose of discussing and improving the Work, but
+//excluding communication that is conspicuously marked or otherwise
+//designated in writing by the copyright owner as "Not a Contribution."
+//
+//"Contributor" shall mean Licensor and any individual or Legal Entity
+//on behalf of whom a Contribution has been received by Licensor and
+//subsequently incorporated within the Work.
+//
+//2. Grant of Copyright License. Subject to the terms and conditions of
+//this License, each Contributor hereby grants to You a perpetual,
+//worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+//copyright license to reproduce, prepare Derivative Works of,
+//publicly display, publicly perform, sublicense, and distribute the
+//Work and such Derivative Works in Source or Object form.
+//
+//3. Grant of Patent License. Subject to the terms and conditions of
+//this License, each Contributor hereby grants to You a perpetual,
+//worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+//(except as stated in this section) patent license to make, have made,
+//use, offer to sell, sell, import, and otherwise transfer the Work,
+//where such license applies only to those patent claims licensable
+//by such Contributor that are necessarily infringed by their
+//Contribution(s) alone or by combination of their Contribution(s)
+//with the Work to which such Contribution(s) was submitted. If You
+//institute patent litigation against any entity (including a
+//cross-claim or counterclaim in a lawsuit) alleging that the Work
+//or a Contribution incorporated within the Work constitutes direct
+//or contributory patent infringement, then any patent licenses
+//granted to You under this License for that Work shall terminate
+//as of the date such litigation is filed.
+//
+//4. Redistribution. You may reproduce and distribute copies of the
+//Work or Derivative Works thereof in any medium, with or without
+//modifications, and in Source or Object form, provided that You
+//meet the following conditions:
+//
+//(a) You must give any other recipients of the Work or
+//Derivative Works a copy of this License; and
+//
+//(b) You must cause any modified files to carry prominent notices
+//stating that You changed the files; and
+//
+//(c) You must retain, in the Source form of any Derivative Works
+//that You distribute, all copyright, patent, trademark, and
+//attribution notices from the Source form of the Work,
+//excluding those notices that do not pertain to any part of
+//the Derivative Works; and
+//
+//(d) If the Work includes a "NOTICE" text file as part of its
+//distribution, then any Derivative Works that You distribute must
+//include a readable copy of the attribution notices contained
+//within such NOTICE file, excluding those notices that do not
+//pertain to any part of the Derivative Works, in at least one
+//of the following places: within a NOTICE text file distributed
+//as part of the Derivative Works; within the Source form or
+//documentation, if provided along with the Derivative Works; or,
+//within a display generated by the Derivative Works, if and
+//wherever such third-party notices normally appear. The contents
+//of the NOTICE file are for informational purposes only and
+//do not modify the License. You may add Your own attribution
+//notices within Derivative Works that You distribute, alongside
+//or as an addendum to the NOTICE text from the Work, provided
+//that such additional attribution notices cannot be construed
+//as modifying the License.
+//
+//You may add Your own copyright statement to Your modifications and
+//may provide additional or different license terms and conditions
+//for use, reproduction, or distribution of Your modifications, or
+//for any such Derivative Works as a whole, provided Your use,
+//reproduction, and distribution of the Work otherwise complies with
+//the conditions stated in this License.
+//
+//5. Submission of Contributions. Unless You explicitly state otherwise,
+//any Contribution intentionally submitted for inclusion in the Work
+//by You to the Licensor shall be under the terms and conditions of
+//this License, without any additional terms or conditions.
+//Notwithstanding the above, nothing herein shall supersede or modify
+//the terms of any separate license agreement you may have executed
+//with Licensor regarding such Contributions.
+//
+//6. Trademarks. This License does not grant permission to use the trade
+//names, trademarks, service marks, or product names of the Licensor,
+//except as required for reasonable and customary use in describing the
+//origin of the Work and reproducing the content of the NOTICE file.
+//
+//7. Disclaimer of Warranty. Unless required by applicable law or
+//agreed to in writing, Licensor provides the Work (and each
+//Contributor provides its Contributions) on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//implied, including, without limitation, any warranties or conditions
+//of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+//PARTICULAR PURPOSE. You are solely responsible for determining the
+//appropriateness of using or redistributing the Work and assume any
+//risks associated with Your exercise of permissions under this License.
+//
+//8. Limitation of Liability. In no event and under no legal theory,
+//whether in tort (including negligence), contract, or otherwise,
+//unless required by applicable law (such as deliberate and grossly
+//negligent acts) or agreed to in writing, shall any Contributor be
+//liable to You for damages, including any direct, indirect, special,
+//incidental, or consequential damages of any character arising as a
+//result of this License or out of the use or inability to use the
+//Work (including but not limited to damages for loss of goodwill,
+//work stoppage, computer failure or malfunction, or any and all
+//other commercial damages or losses), even if such Contributor
+//has been advised of the possibility of such damages.
+//
+//9. Accepting Warranty or Additional Liability. While redistributing
+//the Work or Derivative Works thereof, You may choose to offer,
+//and charge a fee for, acceptance of support, warranty, indemnity,
+//or other liability obligations and/or rights consistent with this
+//License. However, in accepting such obligations, You may act only
+//on Your own behalf and on Your sole responsibility, not on behalf
+//of any other Contributor, and only if You agree to indemnify,
+//defend, and hold each Contributor harmless for any liability
+//incurred by, or claims asserted against, such Contributor by reason
+//of your accepting any such warranty or additional liability.
+//
+//END OF TERMS AND CONDITIONS
+//
+//APPENDIX: How to apply the Apache License to your work.
+//
+//To apply the Apache License to your work, attach the following
+//boilerplate notice, with the fields enclosed by brackets "[]"
+//replaced with your own identifying information. (Don't include
+//the brackets!)  The text should be enclosed in the appropriate
+//comment syntax for the file format. We also recommend that a
+//file or class name and description of purpose be included on the
+//same "printed page" as the copyright notice for easier
+//identification within third-party archives.
+//
+//Copyright [yyyy] [name of copyright owner]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// adapted from :
+// - https://github.com/envoyproxy/envoy/blob/main/api/envoy/type/range.proto
+// - https://github.com/envoyproxy/envoy/blob/main/api/envoy/type/http.proto
+
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// versions:
+// 	protoc-gen-go v1.34.2
+// 	protoc        v3.21.7
+// source: types.proto
+
+package pb
+
+import (
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+type CodecClientType int32
+
+const (
+	CodecClientType_HTTP1 CodecClientType = 0
+	CodecClientType_HTTP2 CodecClientType = 1
+	CodecClientType_HTTP3 CodecClientType = 2
+)
+
+// Enum value maps for CodecClientType.
+var (
+	CodecClientType_name = map[int32]string{
+		0: "HTTP1",
+		1: "HTTP2",
+		2: "HTTP3",
+	}
+	CodecClientType_value = map[string]int32{
+		"HTTP1": 0,
+		"HTTP2": 1,
+		"HTTP3": 2,
+	}
+)
+
+func (x CodecClientType) Enum() *CodecClientType {
+	p := new(CodecClientType)
+	*p = x
+	return p
+}
+
+func (x CodecClientType) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (CodecClientType) Descriptor() protoreflect.EnumDescriptor {
+	return file_types_proto_enumTypes[0].Descriptor()
+}
+
+func (CodecClientType) Type() protoreflect.EnumType {
+	return &file_types_proto_enumTypes[0]
+}
+
+func (x CodecClientType) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use CodecClientType.Descriptor instead.
+func (CodecClientType) EnumDescriptor() ([]byte, []int) {
+	return file_types_proto_rawDescGZIP(), []int{0}
+}
+
+type Int64Range struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// start of the range (inclusive)
+	Start int64 `protobuf:"varint,1,opt,name=start,proto3" json:"start,omitempty"`
+	// end of the range (exclusive)
+	End int64 `protobuf:"varint,2,opt,name=end,proto3" json:"end,omitempty"`
+}
+
+func (x *Int64Range) Reset() {
+	*x = Int64Range{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_types_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Int64Range) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Int64Range) ProtoMessage() {}
+
+func (x *Int64Range) ProtoReflect() protoreflect.Message {
+	mi := &file_types_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Int64Range.ProtoReflect.Descriptor instead.
+func (*Int64Range) Descriptor() ([]byte, []int) {
+	return file_types_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *Int64Range) GetStart() int64 {
+	if x != nil {
+		return x.Start
+	}
+	return 0
+}
+
+func (x *Int64Range) GetEnd() int64 {
+	if x != nil {
+		return x.End
+	}
+	return 0
+}
+
+var File_types_proto protoreflect.FileDescriptor
+
+var file_types_proto_rawDesc = []byte{
+	0x0a, 0x0b, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x12, 0x70,
+	0x6f, 0x6d, 0x65, 0x72, 0x69, 0x75, 0x6d, 0x2e, 0x64, 0x61, 0x73, 0x68, 0x62, 0x6f, 0x61, 0x72,
+	0x64, 0x22, 0x34, 0x0a, 0x0a, 0x49, 0x6e, 0x74, 0x36, 0x34, 0x52, 0x61, 0x6e, 0x67, 0x65, 0x12,
+	0x14, 0x0a, 0x05, 0x73, 0x74, 0x61, 0x72, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x05,
+	0x73, 0x74, 0x61, 0x72, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x65, 0x6e, 0x64, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x03, 0x52, 0x03, 0x65, 0x6e, 0x64, 0x2a, 0x32, 0x0a, 0x0f, 0x43, 0x6f, 0x64, 0x65, 0x63,
+	0x43, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x54, 0x79, 0x70, 0x65, 0x12, 0x09, 0x0a, 0x05, 0x48, 0x54,
+	0x54, 0x50, 0x31, 0x10, 0x00, 0x12, 0x09, 0x0a, 0x05, 0x48, 0x54, 0x54, 0x50, 0x32, 0x10, 0x01,
+	0x12, 0x09, 0x0a, 0x05, 0x48, 0x54, 0x54, 0x50, 0x33, 0x10, 0x02, 0x42, 0x2d, 0x5a, 0x2b, 0x67,
+	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x70, 0x6f, 0x6d, 0x65, 0x72, 0x69,
+	0x75, 0x6d, 0x2f, 0x70, 0x6f, 0x6d, 0x65, 0x72, 0x69, 0x75, 0x6d, 0x2d, 0x63, 0x6f, 0x6e, 0x73,
+	0x6f, 0x6c, 0x65, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
+}
+
+var (
+	file_types_proto_rawDescOnce sync.Once
+	file_types_proto_rawDescData = file_types_proto_rawDesc
+)
+
+func file_types_proto_rawDescGZIP() []byte {
+	file_types_proto_rawDescOnce.Do(func() {
+		file_types_proto_rawDescData = protoimpl.X.CompressGZIP(file_types_proto_rawDescData)
+	})
+	return file_types_proto_rawDescData
+}
+
+var file_types_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_types_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_types_proto_goTypes = []any{
+	(CodecClientType)(0), // 0: pomerium.dashboard.CodecClientType
+	(*Int64Range)(nil),   // 1: pomerium.dashboard.Int64Range
+}
+var file_types_proto_depIdxs = []int32{
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_types_proto_init() }
+func file_types_proto_init() {
+	if File_types_proto != nil {
+		return
+	}
+	if !protoimpl.UnsafeEnabled {
+		file_types_proto_msgTypes[0].Exporter = func(v any, i int) any {
+			switch v := v.(*Int64Range); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: file_types_proto_rawDesc,
+			NumEnums:      1,
+			NumMessages:   1,
+			NumExtensions: 0,
+			NumServices:   0,
+		},
+		GoTypes:           file_types_proto_goTypes,
+		DependencyIndexes: file_types_proto_depIdxs,
+		EnumInfos:         file_types_proto_enumTypes,
+		MessageInfos:      file_types_proto_msgTypes,
+	}.Build()
+	File_types_proto = out.File
+	file_types_proto_rawDesc = nil
+	file_types_proto_goTypes = nil
+	file_types_proto_depIdxs = nil
+}
